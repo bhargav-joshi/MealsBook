e2d0bc4269872c2bc556bf3dc22688f195aa660c
Bhargav Joshi <bhargavjoshi55@gmail.com>
2021-07-03 19:53:14 +0530

    Code added V1.0

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Footer() {
+    return <div style={{backgroundColor:'#000080', color:'white'}}>
+        <p> Designed & Developed by</p>
+        <p>Bhargav Joshi</p>
+    </div>
+}
+
+export default Footer
---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+function Navbar() {
+    return <div style={{backgroundColor:'black', color:'white'}}>
+        <h1 style={{backgroundColor:'black', color:'Yellow'}}>MealsBook</h1>
+        <a href="#" style ={{margin:'10px', color:'white'}}>Home</a>
+        <a href="#" style ={{margin:'10px', color:'white'}}>Order</a>
+        <a href="#" style ={{margin:'10px', color:'white'}}>Contact Us</a>
+    
+    </div>
+}
+export default Navbar
